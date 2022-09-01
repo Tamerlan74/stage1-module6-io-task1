a74db6286bdd2852bc05059ccc98a35a9ac50358
Tamerlan Zhabassov <zhbssv@mail.ru>
2022-09-02 00:03:05 +0600

    do

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,22 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile result = null;
+        try (FileInputStream fis = new FileInputStream(file.getPath())) {
+            try(ObjectInputStream ois = new ObjectInputStream(fis)) {
+                result = (Profile) ois.readObject();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
